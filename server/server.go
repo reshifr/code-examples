b161797943ccca6baa349e63937397912d0c56e5
Renol P. H. <reshifr@gmail.com>
2024-09-01 04:03:09 +0700

    Add HTTPS server and NGINX proxy

---
+++ server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	certFile := os.Getenv("CERT_FILE")
+	keyFile := os.Getenv("KEY_FILE")
+	indexFile := os.Getenv("INDEX_FILE")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		clientIP := r.Header.Get("X-Real-IP")
+		if clientIP == "" {
+			clientIP = r.RemoteAddr
+		}
+		log.Printf("Access from: %s", clientIP)
+		content, err := os.ReadFile(indexFile)
+		if err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(content)
+	})
+
+	log.Println("Starting server on localhost:8443")
+	err = http.ListenAndServeTLS("localhost:8443", certFile, keyFile, nil)
+	if err != nil {
+		log.Fatal("ListenAndServeTLS: ", err)
+	}
+}
